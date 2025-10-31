69188dc078b8b18fe6af6fe87915a725c50ec066
✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>
2025-10-31 19:01:25 +0530

    Update userbot.py
    Signed-off-by: ✰ sᴀᴛʏᴀ ʙᴀʙʏ ✰ <tridevk442@gmail.com>

--- BABYMUSIC/core/userbot.py
+++ BABYMUSIC/core/userbot.py
@@ -23,8 +23,8 @@ class Userbot(Client):
         if config.STRING1:
             await self.one.start()
             try:
-                await self.one.join_chat("https://t.me/+OL6jdTL7JAJjYzVl")
-                await self.one.join_chat("BABY09_WORLD")
+                await self.one.join_chat("https://t.me/+gF7M1_0PC803ZjU9")
+                await self.one.join_chat("https://t.me/YTM_Points")
             except:
                 pass
             assistants.append(1)
